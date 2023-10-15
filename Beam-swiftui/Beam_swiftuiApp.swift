cacbea7a420bd8321f543a40898e05d02806f402
Lakshmipriya02 <yeminenipriya285@gmail.com>
2023-10-15 11:34:05 +0530

    Initial Commit

---
+++ Beam-swiftui/Beam_swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  Beam_swiftuiApp.swift
+//  Beam-swiftui
+//
+//  Created by Priya on 15/10/23.
+//
+
+import SwiftUI
+
+@main
+struct Beam_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
